66f83a91151c04db76153c9a17fa0583c731c666
Bushra <bushragbadegesin@gmail.com>
2026-09-14 08:39:55 +0100

    first practice with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(1)
+
+  const increaseSelection = () => {
+    setCounter(counter + 1)
+  }
+  return (
+    <div className="w-75 bg-amber-600 p-2 flex flex-col items-center text-center rounded-2xl m-4">
+      <img src="src/assets/flower.jpg" alt="flower" />
+      <h2 className="font-bold text-3xl text-amber-900">Spreading love</h2>
+      <h1 className="font-extrabold text-3xl text-amber-900">
+        {counter}
+      </h1>
+      <p className="text-left py-2">
+        Love is all about being thoughtful and intentional towards your partner.
+        Start today by getting them flowers.
+      </p>
+      <button onClick={increaseSelection} className="bg-amber-800 px-3.5 py-1.5 rounded-4xl">Select</button>
+    </div>
+  );
+};
+
+export default App;
